fc7cf7f757687f629e8ed4aa195e66b8ea2b37f5
Sophia <gmb728@gmail.com>
2019-10-04 09:25:12 +0800

    Initial Commit

---
+++ PrincessesTableView_SwfitUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PrincessesTableView_SwfitUI
+//
+//  Created by Chang Sophia on 10/4/19.
+//  Copyright © 2019 Chang Sophia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
