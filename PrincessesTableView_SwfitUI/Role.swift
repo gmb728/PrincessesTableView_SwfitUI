f10bda3b534b8ffc93f62993421fe64a9ee4b24c
Sophia <gmb728@gmail.com>
2019-10-08 10:45:46 +0800

    role.swift

---
+++ PrincessesTableView_SwfitUI/Role.swift
@@ -0,0 +1,19 @@
+//
+//  Role.swift
+//  PrincessesTableView_SwfitUI
+//
+//  Created by Chang Sophia on 10/8/19.
+//  Copyright © 2019 Chang Sophia. All rights reserved.
+//
+
+import Foundation
+struct Role {
+    var name: String
+    var image: String
+    var weapon: String
+    var official: String
+    var number: String
+    var movie: String
+    var pet: String
+}
+    
